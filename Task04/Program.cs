dc0ca8a4f0dca3eb2d47e5b16048efaabb5d66d7
Aleks <kakdela208@mail.ru>
2022-11-27 21:37:05 +1000

    add 4 and 5 Task

---
+++ Task04/Program.cs
@@ -0,0 +1,8 @@
+﻿int nubmer = 5;
+int result = nubmer / 4;
+Console.WriteLine(result);
+
+double result2 = nubmer / 2;
+Console.WriteLine(result2);
+// int resultNormal = result;
+
---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string numUser = Console.ReadLine();
+int num = Convert.ToInt32(numUser);
+
+int lust = num % 10; // % - выдаёт значение (остаток от деления) в данном случае при делении на 10 остаток от 456 - число 6
+int first = num / 100; // так как деление целочисленное, то при делении остаток отбрасывается. 456/100=4,56 в программировании будет равно просто 4
+Console.WriteLine($"Последняя цифра: {lust}");
+Console.WriteLine($"Первая цифра: {first}");
