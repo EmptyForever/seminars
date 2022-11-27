6863a2e7ac10f6590ec14ef42ebd54b7603ab3de
Aleks <kakdela208@mail.ru>
2022-11-27 22:06:36 +1000

    add task 06 - vvod n and chisla ot -n do n

---
+++ Task06/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+// Пользователь вводит число N --- Нужно показать все целые числа между N и -N.
+// Если число положительное, тогда уменьшаем. Отрицательное - увеличиваем, ноль - выдаём ответ, что "это просто ноль"
+
+ Console.Write("Введите число: ");
+string numUser = Console.ReadLine(); // строка, которую вводит пользователь numUser
+int num = Convert.ToInt32(numUser); // переводит строку в 32 битную чо-то там, короче в целое число без запятых
+ int numMinus = num * (-1);
+
+if (num > 0) // если num положительное
+{
+    while (num > numMinus)
+    {
+         Console.Write(num);
+         num = num - 1;
+    }
+}
+else if (num < 0) // если num отрицательное
+{
+    while (num <= numMinus)
+    {
+         Console.Write(num);
+         num = num + 1;
+    }
+}
+else if (num == 0) // если num 0
+{
+    Console.WriteLine(num);
+}
